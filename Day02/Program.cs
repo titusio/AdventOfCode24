15863c77da7cd1f7c0c8fa3cfcb4286d8595643f
titus <titus.huebner@gmail.com>
2024-12-02 07:08:55 +0100

    add solution for puzzle 1 of day 2

---
+++ Day02/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace AdventOfCode.Day02;
+
+public class Program
+{
+    /// <summary>
+    /// Parses the input into a sequence of reports.
+    /// A report is an array of integers.
+    /// </summary>
+    /// <param name="lines">The input lines.</param>
+    /// <returns>The sequence of reports.</returns>
+    private static IEnumerable<int[]> ParseInput(string[] lines)
+    {
+        return lines.Select(l => l.Split(" ", StringSplitOptions.TrimEntries).Select(int.Parse).ToArray());
+    }
+
+    /// <summary>
+    /// Checks if the report is safe.
+    /// A report is safe if all the differences between the numbers are either positive or negative,
+    /// if the differences are not greater than 3 and if all differences are not equal to 0.
+    /// </summary>
+    /// <param name="report">The report.</param>
+    /// <returns>True if the report is safe, false otherwise.</returns>
+    private static bool IsSave(int[] report)
+    {
+        List<int> differences = [];
+        
+        for (int i = 1; i < report.Length; i++)
+        {
+            int diff = report[i] - report[i - 1];
+            differences.Add(diff);
+        }
+        
+        bool positive = differences.Any(d => d > 0);
+        bool negative = differences.Any(d => d < 0);
+        
+        bool invalidDifferences = positive && negative;
+        
+        if (invalidDifferences)
+        {
+            return false;
+        }
+
+        return !differences.Distinct().Any(d => d == 0 || Math.Abs(d) > 3);
+    }
+    
+    public static void Main()
+    {
+        // string input = File.ReadAllText("Day02.txt");
+        string input = """
+                       7 6 4 2 1
+                       1 2 7 8 9
+                       9 7 6 2 1
+                       1 3 2 4 5
+                       8 6 4 4 1
+                       1 3 6 7 9
+                       """;
+        
+        string[] lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+        lines = File.ReadAllLines("Day02.txt");
+        IEnumerable<int[]> reports = ParseInput(lines);
+
+        int correctReports = reports.Count(IsSave);
+        Console.WriteLine($"there are {correctReports} correct reports");
+    }
+}
