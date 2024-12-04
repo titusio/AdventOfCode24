716e6f177251344f507eb0c62cff6d0424ef5751
titus <titus.huebner@gmail.com>
2024-12-04 08:43:58 +0100

    not working part 2 of day 3

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -8,8 +8,23 @@ class Program
     {
         // 161 = (2*4 + 5*5 + 11*8 + 8*5).
         string input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        // input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
         input = File.ReadAllText("Day03.txt");
+        // input = input.Replace(System.Environment.NewLine, "");
 
+        int result = GetMultiplication(input);
+        Console.WriteLine($"The result is {result}"); // 174336360
+
+        const string expression = @"don't\(\).*?do\(\)";
+        Regex regex = new(expression, RegexOptions.Singleline);
+        Console.WriteLine(regex.Matches(input).Count);
+        input = regex.Replace(input, "");
+        result = GetMultiplication(input);
+        Console.WriteLine($"The result is {result}");
+    }
+
+    private static int GetMultiplication(string input)
+    {
         MatchCollection matches = Regex.Matches(input, @"mul\([0-9]*,[0-9]*\)");
 
         int result = 0;
@@ -26,6 +41,6 @@ class Program
             result += a * b;
         }
 
-        Console.WriteLine($"The result is {result}"); // 174336360
+        return result;
     }
 }
