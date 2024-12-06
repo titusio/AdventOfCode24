79acd7bc6d74d1fab7e1bcfdb8f04a9ed3126dfa
titus <titus.huebner@gmail.com>
2024-12-06 07:48:34 +0100

    add part two of day 6

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -51,32 +51,30 @@ class Program
         return grid;
     }
 
-    static void Main()
-    {
-        string input = """
-                       ....#.....
-                       .........#
-                       ..........
-                       ..#.......
-                       .......#..
-                       ..........
-                       .#..^.....
-                       ........#.
-                       #.........
-                       ......#...
-                       """;
 
-        input = File.ReadAllText("Day06.txt");
-        Tile[,] grid = ParseInput(input, out (int x, int y) currentPosition, out Direction currentDirection);
-        HashSet<(int x, int y)> visited = [];
+    private static void FindPath(Tile[,] grid, (int x, int y) currentPosition, Direction currentDirection,
+        out int visitedTiles, out bool hasLoop)
+    {
+        hasLoop = false;
+        visitedTiles = 0;
 
         int width = grid.GetLength(0);
         int height = grid.GetLength(1);
+
+        HashSet<(int x, int y)> visited = [];
+        HashSet<ValueTuple<(int x, int y), Direction>> states = [];
+
         while (currentPosition.x > 0 && currentPosition.x < width && currentPosition.y > 0 &&
                currentPosition.y < height)
         {
             visited.Add(currentPosition);
-            Console.WriteLine($"Current position: {currentPosition}");
+
+            if (!states.Add((currentPosition, currentDirection)))
+            {
+                hasLoop = true;
+                visitedTiles = visited.Count;
+                return;
+            }
 
             switch (currentDirection)
             {
@@ -161,6 +159,54 @@ class Program
             }
         }
 
-        Console.WriteLine($"Visited {visited.Count} tiles");
+        visitedTiles = visited.Count;
+    }
+
+    static void Main()
+    {
+        string input = """
+                       ....#.....
+                       .........#
+                       ..........
+                       ..#.......
+                       .......#..
+                       ..........
+                       .#..^.....
+                       ........#.
+                       #.........
+                       ......#...
+                       """;
+
+        input = File.ReadAllText("Day06.txt");
+        Tile[,] grid = ParseInput(input, out (int x, int y) currentPosition, out Direction currentDirection);
+
+        FindPath(grid, (currentPosition.x, currentPosition.y), currentDirection, out int visitedTiles, out _);
+
+        int width = grid.GetLength(0);
+        int height = grid.GetLength(1);
+
+        int result = 0;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (currentPosition.x == x && currentPosition.y == y)
+                    continue;
+
+                Tile tile = grid[x, y];
+                if (tile == Tile.Occupied) continue;
+
+                grid[x, y] = Tile.Occupied;
+                FindPath(grid, (currentPosition.x, currentPosition.y), currentDirection, out _, out bool hasLoop);
+                // reset tile
+                grid[x, y] = Tile.Empty;
+
+                result += hasLoop ? 1 : 0;
+            }
+        }
+
+        Console.WriteLine($"Visited {visitedTiles} tiles"); // 5305
+        Console.WriteLine($"We can create {result} loops"); // 2143
     }
 }
