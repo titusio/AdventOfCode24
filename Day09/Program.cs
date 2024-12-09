2e3507becb1f76e74d2693900f8c783316c57000
titus <titus.huebner@gmail.com>
2024-12-09 08:35:10 +0100

    add part two of day 9!!

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -23,14 +23,8 @@ class Program
         return ids;
     }
 
-    static void Main(string[] args)
+    private static long ReorderIds(List<int?> ids)
     {
-        string input = "2333133121414131402";
-        input = File.ReadAllText("Day09.txt").Replace("\n", "");
-        ProcessInput(input);
-
-        List<int?> ids = ProcessInput(input);
-
         int index = ids.Count - 1;
 
         while (ids.Count(id => !id.HasValue) != 0)
@@ -56,13 +50,98 @@ class Program
 
         ids = ids.Where(id => id.HasValue).ToList();
 
+        long sum = CalculateChecksum(ids);
+
+        return sum;
+    }
+
+    private static long Compress(List<int?> ids)
+    {
+        int last = ids.Count - 1;
+
+        while (last > 0)
+        {
+            while (ids[last] == null && last >= 0)
+            {
+                last -= 1;
+            }
+
+            // the number we are looking at
+            int number = ids[last]!.Value;
+            // how many times it appears
+            int count = 1;
+
+            while (last > 1 && ids[last - 1].HasValue && ids[last - 1] == number)
+            {
+                count += 1;
+                last -= 1;
+            }
+
+            int start = 0;
+
+            for (start = 0; start < ids.Count; start++)
+            {
+                bool free = true;
+
+                if (start + count >= ids.Count)
+                {
+                    start = last;
+                    break;
+                }
+
+                for (int j = 0; j < count; j++)
+                {
+                    if (ids[start + j] == null) continue;
+                    free = false;
+                    break;
+                }
+
+                if (free) break;
+            }
+
+            if (start >= last)
+            {
+                last -= 1;
+                continue;
+            }
+
+            // set the number at the start and nullify the rest
+            for (int i = 0; i < count; i++)
+            {
+                ids[start + i] = number;
+                ids[last + i] = null;
+            }
+
+            last -= 1;
+        }
+
+        return CalculateChecksum(ids);
+    }
+
+    private static long CalculateChecksum(List<int?> ids)
+    {
         long sum = 0;
 
         for (int i = 0; i < ids.Count; i++)
         {
-            sum += ids[i]!.Value * i;
+            sum += i * (ids[i] ?? 0);
         }
 
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        string input = "2333133121414131402";
+        input = File.ReadAllText("Day09.txt").Replace("\n", "");
+        ProcessInput(input);
+
+        List<int?> ids = ProcessInput(input);
+
+        long sum = ReorderIds(ids.ToList());
         Console.WriteLine(sum);
+
+        long compressed = Compress(ids.ToList());
+        Console.WriteLine(compressed);
     }
 }
